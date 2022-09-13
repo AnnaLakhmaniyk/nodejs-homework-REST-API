869357d37a8c91660d5a8c7bd9b802264915918d
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-09-13 15:21:07 +0300

    changed db

--- src/db/conection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const DB_HOST = process.env.DB_HOST;
-async function conectMongo() {
-  return mongoose.connect(DB_HOST, { dbName: "db-contacts" });
-}
-module.exports = {
-  conectMongo,
-};
